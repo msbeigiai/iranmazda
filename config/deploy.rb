a4397debeba013db5217be034889acae556eb6d6
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-04-24 13:17:48 +0430

    home directory changed in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@bitbucket.org:mohsen662/iran_mazda.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "home/deploy/iran_mazda"
+set :deploy_to, '/home/deploy/iran_mazda'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
