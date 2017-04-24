e28d53a51665a4e9b41715ea85bc37766c942e3c
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-04-24 12:27:30 +0430

    Server in deploy.rb edited

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,14 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '192.69.208.220', user: 'deploy', roles: %w{web app db}
+#server '192.69.208.220', user: 'deploy', roles: %w{web app db}
+server '192.69.208.220',
+  user: 'deploy',
+  roles: %w{web app db},
+  ssh_options: {
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
 
 # server-based syntax
 # ======================
