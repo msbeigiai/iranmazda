cecf2a9398fa706cd1299b5e8932b691f457ff07
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-05-08 11:07:15 +0430

    ordered paginate

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.paginate(:page => params[:page], per_page: 5)
+    @blogs = Blog.paginate(:page => params[:page], per_page: 5).order('id DESC')
   end
 
   # GET /blogs/1
