daff69121d3c633266971c274beeec69bdcda375
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-06-28 09:03:28 +0430

    Added title for SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     options = [ :hard_wrap, :no_intra_emphasis ]
     Markdown::new(text, *options).to_html.html_safe
   end
+
+  def title(text)
+    content_for :title, text
+  end
 end
